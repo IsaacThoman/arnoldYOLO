22c378532807acc3d0fb003d4275eec4d09dff0f
IsaacThoman <isaacthoman@outlook.com>
2021-05-31 12:12:35 -0400

    Auto refresh- still doesn't use gpu

--- yoloTest/Form1.Designer.cs
+++ yoloTest/Form1.Designer.cs
@@ -33,16 +33,17 @@ namespace yoloTest
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(27, 55);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(455, 315);
+            this.pictureBox1.Size = new System.Drawing.Size(640, 360);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -67,19 +68,48 @@ namespace yoloTest
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(326, 378);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Webcam";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(501, 376);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(118, 19);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Webcam enabled";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(557, 450);
+            this.ClientSize = new System.Drawing.Size(695, 461);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -88,7 +118,9 @@ namespace yoloTest
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- yoloTest/Form1.cs
+++ yoloTest/Form1.cs
@@ -12,22 +12,21 @@ using Alturos.Yolo.Model;
 using OpenCvSharp.Extensions;
 using OpenCvSharp;
 
+using Emgu.CV;
+using Emgu.CV.Structure;
+using ImageCapture;
+
 namespace yoloTest
 {
     public partial class Form1 : Form
     {
         public static string imageLocation = "";
+        bool _streaming;
 
 
-
-        public Form1()
+        public void yoloThingy()
         {
-            InitializeComponent();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            pictureBox1.Image = Image.FromFile(imageLocation);
+            //  pictureBox1.Image = Image.FromFile(imageLocation);
 
             var configurationDetector = new YoloConfigurationDetector();
             var config = configurationDetector.Detect();
@@ -58,12 +57,12 @@ namespace yoloTest
                         var pen = new Pen(Color.Red, 5);
                         var point = new System.Drawing.Point(x, y);
 
-                      //  if (item.Type == "person")
-                  //      {
+                    //    if (item.Type == "person")
+                   //     {
                             graphics.DrawRectangle(pen, rectangle);
                             string thisBoxString = item.Type + Environment.NewLine + "Confidence: " + item.Confidence;
                             graphics.DrawString(thisBoxString, font, brush, point);
-                     //   }
+                    //    }
 
 
                     }
@@ -74,8 +73,17 @@ namespace yoloTest
 
                 }
             }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
 
 
+            yoloThingy();
 
         }
 
@@ -95,6 +103,69 @@ namespace yoloTest
             }
         }
 
+        private static Webcam camera = new Webcam(new System.Drawing.Size(1280, 720), 1);
+        private static bool cameraOn = false;
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+     //       if (cameraOn == false)
+     //       {
+                camera.Start();
+                Thread.Sleep(1000);
+                cameraOn = true;
+         //   }
+
+            Image captured_image = camera.currentImage;
+            pictureBox1.Image = captured_image;
+           // camera.Stop();
+            yoloThingy();
+            camera.Stop();
+            cameraOn = false;
+           
+
+        }
+        public bool runningYOLO = false;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (runningYOLO == false) {
+                runningYOLO = true;
+                if (cameraOn == false)
+                {
+                    camera.Start();
+                    Thread.Sleep(1000);
+                    cameraOn = true;
+                }
+
+                Image captured_image = camera.currentImage;
+                pictureBox1.Image = captured_image;
+                // camera.Stop();
+                yoloThingy();
+                runningYOLO = false;
+
+            }
+          
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            camera.Stop();
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                timer1.Enabled = true;
+            }
+            else
+            {
+                timer1.Enabled = false;
+                camera.Stop();
+                cameraOn = false;
+            }
+        }
     }
 }
